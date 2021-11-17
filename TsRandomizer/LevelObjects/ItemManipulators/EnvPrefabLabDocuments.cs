1ff2b587468334573808f132d342fcba6079ee70
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2021-11-16 20:08:05 -0700

    Add stub for Lab documents

---
+++ TsRandomizer/LevelObjects/ItemManipulators/EnvPrefabLabDocuments.cs
@@ -0,0 +1,34 @@
+﻿// using System;
+// using System.Linq;
+// using Timespinner.GameAbstractions.GameObjects;
+// using Timespinner.GameAbstractions.Gameplay;
+// using Timespinner.GameAbstractions.HUD;
+// using Timespinner.GameAbstractions.Inventory;
+using Timespinner.GameObjects.BaseClasses;
+// using TsRandomizer.Extensions;
+using TsRandomizer.IntermediateObjects;
+using TsRandomizer.Randomisation;
+using TsRandomizer.Screens;
+
+namespace TsRandomizer.LevelObjects.ItemManipulators
+{
+	[TimeSpinnerType("Timespinner.GameObjects.Events.EnvironmentPrefabs.L11_Lab.EnvPrefabLabHistoricalDocuments")]
+	// ReSharper disable once UnusedMember.Global
+	class EnvPrefabLabDocuments : ItemManipulator
+	{
+		public EnvPrefabLabDocuments(Mobile typedObject, ItemLocation itemLocation) : base(typedObject, itemLocation)
+		{
+
+		}
+
+		protected override void Initialize(SeedOptions options)
+		{
+
+		}
+
+		protected override void OnUpdate(GameplayScreen gameplayScreen)
+		{
+			
+		}
+	}
+}
