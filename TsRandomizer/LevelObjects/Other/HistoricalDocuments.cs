e03e83a0702f66fa71bc14426e3b671ff8a65541
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2021-11-17 12:07:51 -0700

    Move historical documents

---
+++ TsRandomizer/LevelObjects/Other/HistoricalDocuments.cs
@@ -0,0 +1,19 @@
+﻿using Timespinner.GameObjects.BaseClasses;
+using TsRandomizer.IntermediateObjects;
+
+namespace TsRandomizer.LevelObjects.Other
+{
+	[TimeSpinnerType("Timespinner.GameObjects.Events.EnvironmentPrefabs.L11_Lab.EnvPrefabLabHistoricalDocuments")]
+	// ReSharper disable once UnusedMember.Global
+	class HistoricalDocuments: LevelObject
+	{
+		public HistoricalDocuments(Mobile typedObject) : base(typedObject)
+		{
+			typedObject.IsFlippedVertically = true;
+		}
+
+		protected override void Initialize(SeedOptions options)
+		{
+		}
+	}
+}
