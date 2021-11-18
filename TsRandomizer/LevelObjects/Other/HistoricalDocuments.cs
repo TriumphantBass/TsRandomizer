5fe9aead3eaf66ce08f4333061c997ee53b635f4
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2021-11-17 20:11:37 -0700

    Reverting dialog that doesn't really fit

--- TsRandomizer/LevelObjects/Other/HistoricalDocuments.cs
+++ TsRandomizer/LevelObjects/Other/HistoricalDocuments.cs
@@ -10,16 +10,8 @@ namespace TsRandomizer.LevelObjects.Other
 	{
 		public HistoricalDocuments(Mobile typedObject) : base(typedObject)
 		{
-			// Without MC
 			TimeSpinnerGame.Localizar.OverrideKey("q_ram_4_lun_29alt",
 				"It says, 'Redacted Temporal Research: Lord of Ravens'. Maybe I should ask the crow about this...");
-			// With MC
-			TimeSpinnerGame.Localizar.OverrideKey("q_ram_4_lun_29",
-				"It says, 'Redacted Temporal Research: Lord of Ravens'...");
-			TimeSpinnerGame.Localizar.OverrideKey("q_ram_4_lun_30",
-				"Merchant Crow, do you know anything about this?");
-			TimeSpinnerGame.Localizar.OverrideKey("q_ram_4_lun_31",
-				"Hmm oh... wow... some sort of rift is opening");
 		}
 
 		protected override void Initialize(SeedOptions options)
