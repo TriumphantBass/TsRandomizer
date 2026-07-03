9c55f803646b9f4555f009c3fc55ebdc60ab9885
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2026-07-03 02:05:05 -0700

    Scaffolding for Seykis shop

---
+++ TsRandomizer/LevelObjects/Other/SeykisNPC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Timespinner.GameAbstractions.Inventory;
+using Timespinner.GameObjects.BaseClasses;
+using TsRandomizer.Extensions;
+using TsRandomizer.IntermediateObjects;
+using TsRandomizer.Screens;
+using TsRandomizer.Settings;
+
+namespace TsRandomizer.LevelObjects.Other
+{
+	[TimeSpinnerType("Timespinner.GameObjects.NPCs.QuartermasterNPC")]
+	class SeykisNpc : LevelObject
+	{
+		MerchantInventory merchandiseInventory = new MerchantInventory();
+
+		public SeykisNpc(Mobile typedObject, GameplayScreen gameplayScreen) : base(typedObject, gameplayScreen)
+		{
+		}
+
+		protected override void Initialize(Seed seed, SettingCollection settings)
+		{
+			merchandiseInventory.AddItem(EInventoryRelicType.TimespinnerGear3);
+			merchandiseInventory.AddItem(EInventoryEquipmentType.Sunglasses);
+			merchandiseInventory.AddItem(EInventoryFamiliarType.Meyef);
+			merchandiseInventory.AddItem(EInventoryOrbType.Flame, EOrbSlot.Melee);
+			merchandiseInventory.AddItem(EInventoryOrbType.Barrier, EOrbSlot.Spell);
+			merchandiseInventory.AddItem(EInventoryOrbType.Gun, EOrbSlot.Passive);
+			merchandiseInventory.AddItem(EInventoryUseItemType.HiSandBottle);
+			merchandiseInventory.AddItem(EInventoryEquipmentType.SecurityVisor);
+			Dynamic.OpenShop(NPCBase.ENPCType.CultistPriest, merchandiseInventory);
+		}
+	}
+}
